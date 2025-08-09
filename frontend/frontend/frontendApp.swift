49d6509a2bdff1f37cc6847c7ed4ca83eae995b8
Callum Sharrock <38599348+callum-sh@users.noreply.github.com>
2025-08-09 13:48:18 -0700

    init new project

---
+++ frontend/frontend/frontendApp.swift
@@ -0,0 +1,20 @@
+//
+//  frontendApp.swift
+//  frontend
+//
+//  Created by callum on 2025-08-09.
+//
+
+import SwiftUI
+
+@main
+struct frontendApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
